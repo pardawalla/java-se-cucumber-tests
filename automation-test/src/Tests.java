355ac2117775f0c7d2a275e6f4861d1db1cd8eb3
pardawalla <104052542+pardawalla@users.noreply.github.com>
2022-12-28 19:17:31 +1300

    able to get Keyword, Make, Model and Body Style fields from https://www.trademe.co.nz/a/motors

--- automation-test/src/Tests.java
+++ automation-test/src/Tests.java
@@ -1,16 +1,17 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.Reporter;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-import java.time.Duration;
 import java.lang.System;
+import java.time.Duration;
+
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 
 
@@ -19,19 +20,23 @@ public class Tests {
     
     //public WebDriver object so that it can be used anywhere within class1
     public WebDriver driver; 
+    WebDriverWait wait;
     
     @BeforeClass
     public void Setup()
     {
+        Long timeOutInMilliseconds = (long) 500;
         System.setProperty("webdriver.chrome.driver", "/Users/hussain/repos/java-se-cucumber-tests/drivers/chromedriver");
         driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        wait = new WebDriverWait(driver, Duration.ofMillis(timeOutInMilliseconds));
     }
 
-    // @AfterSuite
-    // public void TearDown()
-    // {
-    //     driver.quit();
-    // }
+    @AfterSuite
+    public void TearDown()
+    {
+        driver.quit();
+    }
 	
 	@Test (priority = 0)
 	public void OpenBrowser() 
@@ -48,8 +53,24 @@ public class Tests {
     @Test (priority = 1)
 	public void TradmeMotors() 
     {
-		Reporter.log("This test verifies the current selenium compatibility with TestNG by launching the chrome driver");
+		driver.get("https://www.trademe.co.nz/");
 	    driver.findElement(By.linkText("Motors")).click();
+        String expectedTitle = "Cars And Vehicles For Sale | Trade Me Motors";
+        wait.until(ExpectedConditions.titleContains(expectedTitle));
+	    String originalTitle = driver.getTitle();
+	    Assert.assertEquals(originalTitle, expectedTitle);
+
+        driver.findElement(By.linkText("Used")).click();
+        //driver.findElement(By.id("tg-517106a7-79e9-51dd-9bf6-765f33bfec15" )).click();
+        WebElement keywords =  driver.findElement(By.name("keyword"));
+        keywords.sendKeys("blue");
+        WebElement make = driver.findElement(By.name("selectedMake"));
+        WebElement model = driver.findElement(By.name("searchParams.model"));
+        WebElement bodyStyle = driver.findElement(By.className("tm-motors-search-bar__dropdown-multi-select-text"));
+
+
+        Reporter.log("Testing so far");
+
     } 
 
 }
